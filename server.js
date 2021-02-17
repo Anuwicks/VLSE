a60400c4dac90263623cb649f4d24f1bc40f1b0a
tawic1 <tawic1@student.monash.edu>
2021-02-18 03:08:56 +1100

    babel fix for server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 require("dotenv").config();
 
+//Initialise express erver
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
