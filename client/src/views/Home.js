9967b0c18ca671e434db3c72a5f1f5f4565ef6e1
tawic1 <tawic1@student.monash.edu>
2021-02-18 16:48:26 +1100

    Adding in site navbar

---
+++ client/src/views/Home.js
@@ -0,0 +1,15 @@
+import { Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+
+import AppBar from "../components/AppBar";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <AppBar />
+    </div>
+  );
+}
